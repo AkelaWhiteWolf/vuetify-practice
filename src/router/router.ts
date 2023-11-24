fa79e2f0d0dcdba431d344403af1e6e4181b376b
Alexey Shvets <alexey199822la@gmail.com>
2023-11-24 14:55:06 +0200

    dev/document title changed with page

--- src/router/router.ts
+++ src/router/router.ts
@@ -8,17 +8,31 @@ export const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomePage
+      component: HomePage,
+      meta: {
+        title: 'Home'
+      }
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('@/pages/AboutPage.vue')
+      component: () => import('@/pages/AboutPage.vue'),
+      meta: {
+        title: 'About'
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/pages/LoginPage.vue')
+      component: () => import('@/pages/LoginPage.vue'),
+      meta: {
+        title: 'Login'
+      }
     }
   ]
+});
+
+router.beforeEach((to, _, next) => {
+  document.title = (to.meta.title as string) || 'Vuetify Practice';
+  next();
 });
