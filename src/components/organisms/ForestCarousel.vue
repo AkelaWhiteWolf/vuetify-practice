c5d4181f8ce3d1d35474dc836c1fbaa6fd789cf8
Alexey Shvets <alexey199822la@gmail.com>
2023-11-24 14:03:52 +0200

    dev/images moved to src/assets folder

--- src/components/organisms/ForestCarousel.vue
+++ src/components/organisms/ForestCarousel.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script setup lang="ts">
-import forest1 from '@public/images/forest-1.jpg';
-import forest2 from '@public/images/forest-2.jpg';
-import forest3 from '@public/images/forest-3.jpg';
+import forest1 from '@/assets/images/forest-1.jpg';
+import forest2 from '@/assets/images/forest-2.jpg';
+import forest3 from '@/assets/images/forest-3.jpg';
 
 const images = [forest1, forest2, forest3];
 </script>
